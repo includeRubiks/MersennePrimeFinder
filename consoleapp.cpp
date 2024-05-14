bf6261f89028fab85f74518877d63148cc698252
includeRubiks <159594845+includeRubiks@users.noreply.github.com>
2024-05-14 17:07:23 -0400

    First (and major) update

---
+++ consoleapp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+/*This program was written by includeRubiks on GitHub. Should a world record mersenne prime be found using this program,
+includeRubiks will take 50% of the credit.*/
+vector <int> factors;
+void findFactors(int number) {
+    for(int i = 1; i <= number; i++)
+    {
+        if(number % i == 0){
+            factors.push_back(i);
+        }
+    }
+}
+
+int main() {
+
+    int number = 1; //The number being tested
+    int pe = 1; //Declare the previous exponent variable (a way of speeding things up when doing powers of 2)
+    while(true) {
+        cout << "Testing: " << number << "\n Factors: ";
+        findFactors(number);
+        for(int num : factors){
+            cout << num << " ";
+        }
+        cout << "\n";
+        factors.clear();
+        number++;
+    }
+}
